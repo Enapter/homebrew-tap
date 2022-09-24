194dfea9663e809bb7579eddca0d9fc5d61535ce
Nikolay V. Krasko <nikolay@enapter.com>
2022-09-25 02:04:31 +0300

    adding correct checksum

--- Casks/flasher.rb
+++ Casks/flasher.rb
@@ -1,6 +1,6 @@
 cask "flasher" do
   version "0.0.1-dev.2"
-  sha256 "fc4c58f81b938a629118af3200a26797f93aae375f4d68d83191ac8d517e068f"
+  sha256 "f520d588854b009d4c255602eb057a07c0c8793e4f8f25b0d6b8ed280cf97249"
 
   url "https://downloads.enapter.com/enapter-flasher/#{version}/Enapter%20UCM%20Kit%20Flasher-#{version}-universal.dmg"
   name "Enapter UCM Kit Flasher"
