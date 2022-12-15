6919d038db72be42e1cfcebe37e7b605582cd2f2
Nikolay V. Krasko <nikolay@enapter.com>
2022-12-15 14:35:49 +0400

    bump version of enapter ucm kit flasher to rc.6

--- Casks/flasher.rb
+++ Casks/flasher.rb
@@ -1,6 +1,6 @@
 cask "flasher" do
-  version "1.0.0-rc.3"
-  sha256 "348b8369145ee20f0953ac5e9373e47a9a3e10a4282f3d787353b975fc6df7ce"
+  version "1.0.0-rc.6"
+  sha256 "681367ed3cf4bea46d0bb304f2c41a2f94a99c625dc9a7bbdbd72540ccba8e65"
 
   url "https://downloads.enapter.com/enapter-flasher/#{version}/Enapter%20UCM%20Kit%20Flasher-#{version}-universal.dmg"
   name "Enapter UCM Kit Flasher"
